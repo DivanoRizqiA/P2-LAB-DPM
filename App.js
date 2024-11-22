107c57915ba19eb2552849355a82484b050fc1d9
DivanoRizqiA <divanorizqianandra@student.uir.ac.id>
2024-11-22 17:57:16 +0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet, Alert, TouchableOpacity } from 'react-native';
 
-export default function App() {
+const App = () => {
+  const showAlert = (boxNumber) => {
+    Alert.alert(`Kotak ${boxNumber} ditekan!`);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Tugas 2 Praktikum DPM</Text>
+      <View style={styles.boxContainer}>
+        <TouchableOpacity style={styles.box1} onPress={() => showAlert(1)} />
+        <TouchableOpacity style={styles.box2} onPress={() => showAlert(2)} />
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#D3D3D3', // Warna abu muda
+    justifyContent: 'center', // Mengatur konten di tengah secara vertikal
+    alignItems: 'center', // Mengatur konten di tengah secara horizontal
+  },
+  title: {
+    fontSize: 24, // Ukuran font 24
+    color: 'blue', // Warna biru
+    fontWeight: 'bold', // Menjadikan teks tebal
+    marginBottom: 20, // Jarak antara teks dan kotak
+  },
+  boxContainer: {
+    flexDirection: 'row', // Mengatur arah flex menjadi horizontal
+  },
+  box1: {
+    width: 100,
+    height: 100,
+    backgroundColor: '#FF5733', // Warna kotak 1
+    marginRight: 20, // Jarak antara kotak
+    justifyContent: 'center', // Mengatur konten di tengah secara vertikal
+    alignItems: 'center', // Mengatur konten di tengah secara horizontal
+  },
+  box2: {
+    width: 100,
+    height: 100,
+    backgroundColor: '#33FF57', // Warna kotak 2
   },
 });
+
+export default App;
